541f0e72ce90049ba31dcbe83ef2fcc13922a531
Marco Fernandez Pranno <mfernandezpranno@gmail.com>
2017-09-03 20:20:55 +0200

    Redux basics

--- 02-redux-basics/redux-basics/App.js
+++ 02-redux-basics/redux-basics/App.js
@@ -2,29 +2,33 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Provider } from 'react-redux';
 
+import store from './app/store';
 import {styles} from './app/styles/App';
 import {suma, resta} from './app/actions/counterActions';
 import Contador from './app/components/Contador';
 
 const App = ({valor, suma, resta}) => (
-  <View style={styles.container}>
-    <Contador valor={valor} />
+  <Provider store={store}>
+    <View style={styles.container}>
+      <Contador valor={valor} />
 
-    <View style={styles.botones}>
-      <Button
-        onPress={suma}
-        title="Suma"
-        color="#841584"
-      />
+      <View style={styles.botones}>
+        <Button
+          onPress={suma}
+          title="Suma"
+          color="#841584"
+        />
 
-      <Button
-        onPress={resta}
-        title="Resta"
-        color="#841584"
-      />
+        <Button
+          onPress={resta}
+          title="Resta"
+          color="#841584"
+        />
+      </View>
     </View>
-  </View>
+  </Provider>
 );
 
 const mapDispatchToProps = (dispatch) => {
