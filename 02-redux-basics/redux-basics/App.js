bbc7a88f8d91b1f3dcb08f20878767038ec6614a
Marco Fernandez Pranno <mfernandezpranno@gmail.com>
2017-09-03 20:27:38 +0200

    Redux basics

--- 02-redux-basics/redux-basics/App.js
+++ 02-redux-basics/redux-basics/App.js
@@ -1,45 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+
 import { Provider } from 'react-redux';
-
 import store from './app/store';
-import {styles} from './app/styles/App';
-import {suma, resta} from './app/actions/counterActions';
-import Contador from './app/components/Contador';
 
-const App = ({valor, suma, resta}) => (
+const App = () => (
   <Provider store={store}>
-    <View style={styles.container}>
-      <Contador valor={valor} />
-
-      <View style={styles.botones}>
-        <Button
-          onPress={suma}
-          title="Suma"
-          color="#841584"
-        />
-
-        <Button
-          onPress={resta}
-          title="Resta"
-          color="#841584"
-        />
-      </View>
-    </View>
+    <CounterView />
   </Provider>
 );
 
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({
-    suma,
-    resta
-  }, dispatch);
-};
-
-const mapStateToProps = ({contador: {valor}}) => ({
-  valor
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
---
+++ 02-redux-basics/redux-basics/app/containers/CounterView.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import {styles} from './../styles/App';
+import {suma, resta} from './../actions/counterActions';
+import Contador from './../components/Contador';
+
+const CounterView = ({valor, suma, resta}) => (
+  <Provider store={store}>
+    <View style={styles.container}>
+      <Contador valor={valor} />
+
+      <View style={styles.botones}>
+        <Button
+          onPress={suma}
+          title="Suma"
+          color="#841584"
+        />
+
+        <Button
+          onPress={resta}
+          title="Resta"
+          color="#841584"
+        />
+      </View>
+    </View>
+  </Provider>
+);
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    suma,
+    resta
+  }, dispatch);
+};
+
+const mapStateToProps = ({contador: {valor}}) => ({
+  valor
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterView);
